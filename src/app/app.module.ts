e72463e2af2475af3039560a5a4bdb86b542ce54
wtt161 <wtt161@localhost.localdomain>
2018-10-30 20:32:03 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemsComponent } from './recipes/recipe-list/recipe-items/recipe-items.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemsComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule
