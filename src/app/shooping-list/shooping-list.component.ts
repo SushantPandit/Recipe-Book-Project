de931376485a92fd02a0786835084478d27e20ee
wtt161 <wtt161@localhost.localdomain>
2018-11-01 19:09:01 +0530

    shoping list page working on that

--- src/app/shooping-list/shooping-list.component.ts
+++ src/app/shooping-list/shooping-list.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shooping-list.component.css']
 })
 export class ShoopingListComponent implements OnInit {
+  data: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addRecord(name, amount) {
+    this.data.push({
+      Iname: name,
+      Iamount: amount
+    });
+    console.log(this.data);
+  }
 }
